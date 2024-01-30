be2354a5da079618b24ea241eab67410f6543863
DevNakum <ndev2003@gmail.com>
2024-01-30 17:15:05 +0530

    Lambda expression demo added

---
+++ API training/CSharp Advanced/LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaExpression
+{
+    /// <summary>
+    /// class which handle lambda expression
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// class which handle lambda expression
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // Action method which is doesn't have return statement - without parameter
+            Action welcome = () => Console.WriteLine("Welcome !!");
+            welcome();
+
+            //Action method with parameter
+            Action<string> printName = name => Console.WriteLine($"Hello {name}");
+            printName("Dev");
+
+            // function 
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(4));
+
+            // travsers the list
+            List<int> lstInt = new List<int>() { 1, 2, 3, 4, 5 };
+            lstInt.ForEach(x => Console.Write(x));  
+
+            // predicate - returns true or false
+            Predicate<int> isEven = x => x%2 == 0;
+            Console.WriteLine(isEven(5));
+
+        }
+    }
+}

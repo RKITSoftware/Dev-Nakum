bbb1808dde825a5a9ebf65fc4ff4251d2f586b97
DevNakum <ndev2003@gmail.com>
2024-02-01 13:18:59 +0530

    RSA Demo Added

---
+++ API training/CSharp Advanced/Cryptography/Cryptography/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cryptography
+{
+    /// <summary>
+    /// main class for executing the program
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method take input from user and call the function
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Take input from the user
+            Console.WriteLine("Enter a plain text");
+            string plainText = Console.ReadLine();
+
+            // object of the RSA
+            RSA objRSA = new RSA();
+
+            // encrypted string 
+            string encryptedString = objRSA.Encrypt(plainText);
+            Console.WriteLine($"encrypted string is : {encryptedString}");
+            Console.WriteLine();
+
+            // Decrypted string = plain text
+            string decryptedString = objRSA.Decrypt(encryptedString);
+            Console.WriteLine($"Decrypted string is : {decryptedString}");
+        } 
+    }
+}
---
+++ API training/CSharp Advanced/Cryptography/Cryptography/RSA.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Cryptography
+{
+    /// <summary>
+    ///     encrypt and decrypt the plain text
+    /// </summary>
+    public class RSA
+    {
+        #region Private Member
+        private static RSACryptoServiceProvider _objRSA;
+        #endregion
+
+        #region Constructor
+        public RSA()
+        {
+            _objRSA = new RSACryptoServiceProvider();
+        }
+        #endregion
+
+        #region Public Method
+        
+        /// <summary>
+        /// Encryption of plain text
+        /// </summary>
+        /// <param name="plainText">user's plain text</param>
+        /// <returns>encrypted </returns>
+        public string Encrypt(string plainText)
+        {
+            byte[] plainBytes = Encoding.UTF8.GetBytes(plainText);
+            byte[] encryptedByte = _objRSA.Encrypt(plainBytes, true);
+
+            return Convert.ToBase64String(encryptedByte);                                  
+        }
+
+        /// <summary>
+        /// Decryption of encrypted text
+        /// </summary>
+        /// <param name="ecncryptedString"></param>
+        /// <returns></returns>
+        public string Decrypt(string encryptedString)
+        {
+            byte[] encryptedTextByte = Convert.FromBase64String(encryptedString);
+            byte[] descryptedBytes = _objRSA.Decrypt(encryptedTextByte, true);
+
+            return Encoding.UTF8.GetString(descryptedBytes);
+        }
+        #endregion
+    }
+}

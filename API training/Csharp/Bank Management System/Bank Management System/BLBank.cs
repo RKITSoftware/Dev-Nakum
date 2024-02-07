8e09b2aabc44ec169266cb7ec7e5ef0118182d00
DevNakum <ndev2003@gmail.com>
2024-02-07 11:05:47 +0530

    Final demo modified

---
+++ API training/Csharp/Bank Management System/Bank Management System/BLBank.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Data;
+using System.IO;
+
+namespace Bank_Management_System
+{
+    /// <summary>
+    /// Perform all the operation of bank related
+    /// </summary>
+    public class BLBank
+    {
+        #region Private Member
+        private static int _id = 1;
+        #endregion
+
+        #region Public Properties
+        // Public properties for user details
+        public int id = 1;
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        #endregion
+
+        #region Public Method
+
+        /// <summary>
+        /// Create a new bank account and add it to the DataTable.
+        /// </summary>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="firstName">First name of the account holder.</param>
+        /// <param name="lastName">Last name of the account holder.</param>
+        /// <param name="email">Email of the account holder.</param>
+        /// <param name="phone">Phone number of the account holder.</param>
+        public void CreateAccount(DataTable dataTable, string firstName, string lastName, string email, string phone)
+        {
+            // If DataTable columns don't exist, create them
+            if (!ColumnExists(dataTable, "FirstName"))
+            {
+                // Define data columns
+                DataColumn columnUserId = new DataColumn("UserId", typeof(int));
+                DataColumn columnFName = new DataColumn("FirstName", typeof(string));
+                DataColumn columnLName = new DataColumn("LastName", typeof(string));
+                DataColumn columnEmail = new DataColumn("Email", typeof(string));
+                DataColumn columnPhone = new DataColumn("Phone", typeof(string));
+                DataColumn columnMoney = new DataColumn("Money", typeof(int));
+
+                // Add columns to DataTable
+                dataTable.Columns.Add(columnUserId);
+                dataTable.Columns.Add(columnFName);
+                dataTable.Columns.Add(columnLName);
+                dataTable.Columns.Add(columnEmail);
+                dataTable.Columns.Add(columnPhone);
+                dataTable.Columns.Add(columnMoney);
+
+                // Set UserId as the primary key
+                DataColumn[] assignPrimaryKey = { dataTable.Columns["UserId"] };
+                dataTable.PrimaryKey = assignPrimaryKey;
+            }
+
+            // Create a new DataRow and populate it with user details
+            DataRow row = dataTable.NewRow();
+            UserId = _id;
+            row["UserId"] = _id++;
+            row["FirstName"] = firstName;
+            row["LastName"] = lastName;
+            row["Email"] = email;
+            row["Phone"] = phone;
+            row["Money"] = 0;       // Initial account balance is set to 0
+
+            // Add the DataRow to the DataTable
+            dataTable.Rows.Add(row);
+
+            Console.WriteLine("Your account created successfully");
+
+            // Display the current user data
+            DisplayCurrentUserData(dataTable, UserId);
+        }
+
+        /// <summary>
+        /// Deposit money into the user's account.
+        /// </summary>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="money">Amount of money to deposit.</param>
+        public void DepositMoney(DataTable dataTable, int money)
+        {
+            Console.WriteLine("Money Started Deposit");
+
+            // Find the DataRow based on UserId
+            DataRow currentUser = dataTable.Rows.Find(UserId);
+            if (currentUser != null)
+            {
+                // Update the money balance and display the current user data
+                money += (int)currentUser["Money"];
+
+                currentUser["Money"] = money;
+                DisplayCurrentUserData(dataTable, UserId);
+            }
+            else
+            {
+                Console.WriteLine("Account does not exists");
+            }
+        }
+
+        /// <summary>
+        /// Withdraw money from the user's account.
+        /// </summary>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="money">Amount of money to withdraw.</param>
+        public void WithdrawMoney(DataTable dataTable, int money)
+        {
+            Console.WriteLine("Money Started Withdraw");
+            // Find the DataRow based on UserId
+            DataRow currentUser = dataTable.Rows.Find(UserId);
+            if (currentUser != null)
+            {
+                // Check if sufficient balance exists, update the money balance, and display the current user data
+                if ((int)currentUser["Money"] < money)
+                {
+                    Console.WriteLine("Insufficient Balance");
+                }
+                else
+                {
+                    currentUser["Money"] = (int)currentUser["Money"] - money;
+                    DisplayCurrentUserData(dataTable, UserId);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Account does not exists");
+            }
+        }
+
+        /// <summary>
+        /// Close the user's account by deleting the DataRow from the DataTable.
+        /// </summary>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        public void CloseAccount(DataTable dataTable)
+        {
+            // Find the DataRow based on UserId
+            DataRow currentUser = dataTable.Rows.Find(UserId);
+            if (currentUser != null)
+            {
+                // Delete the DataRow and accept changes to the DataTable
+                currentUser.Delete();
+                dataTable.AcceptChanges();
+
+                Console.WriteLine();
+                Console.WriteLine("your account is successfully deleted");
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine("Account does not exists");
+            }
+
+        }
+
+        /// <summary>
+        /// Check if a column exists in the DataTable.
+        /// </summary>
+        /// <param name="dataTable">DataTable to check.</param>
+        /// <param name="columnName">Name of the column to check.</param>
+        /// <returns>True if the column exists, otherwise false.</returns>
+        public static bool ColumnExists(DataTable dataTable, string columnName)
+        {
+            return dataTable.Columns.Contains(columnName);
+        }
+
+        /// <summary>
+        /// Display the current user's data.
+        /// </summary>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="id">UserId of the current user.</param>
+        public static void DisplayCurrentUserData(DataTable dataTable, int id)
+        {
+            // Find the DataRow based on UserId
+            DataRow currentUser = dataTable.Rows.Find(id);
+            Console.WriteLine();
+            Console.WriteLine("*** Current user data ***");
+            Console.WriteLine();
+            Console.WriteLine($"UserId : {currentUser["UserId"]}");
+            Console.WriteLine($"FirstName : {currentUser["FirstName"]}");
+            Console.WriteLine($"LastName : {currentUser["LastName"]}");
+            Console.WriteLine($"Email : {currentUser["Email"]}");
+            Console.WriteLine($"Phone : {currentUser["Phone"]}");
+            Console.WriteLine($"Money : {currentUser["Money"]}");
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Display all user data in the DataTable and write it to a file.
+        /// </summary>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        public static void DisplayAllUserData(DataTable dataTable)
+        {
+            // Iterating through the DataTable to display user data
+            foreach (DataRow dataRow in dataTable.Rows)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"UserId : {dataRow["UserId"]}");
+                Console.WriteLine($"FirstName : {dataRow["FirstName"]}");
+                Console.WriteLine($"LastName : {dataRow["LastName"]}");
+                Console.WriteLine($"Email : {dataRow["Email"]}");
+                Console.WriteLine($"Phone : {dataRow["Phone"]}");
+                Console.WriteLine($"Money : {dataRow["Money"]}");
+                Console.WriteLine();
+            }
+
+            // Write user data to a file
+            WriteDataIntoFile(dataTable);
+        }
+
+        /// <summary>
+        /// Write all user data in the DataTable to a file.
+        /// </summary>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        public static void WriteDataIntoFile(DataTable dataTable)
+        {
+            // Define the file path for writing data
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\display.txt");
+
+            // Write user data to the file
+            try
+            {
+                // streamWrite for write something on file
+               using (StreamWriter streamWriter = new StreamWriter(filePath))
+               {
+                    foreach (DataRow dataRow in dataTable.Rows)
+                    {
+                        streamWriter.WriteLine();
+                        streamWriter.WriteLine($"UserId : {dataRow["UserId"]}");
+                        streamWriter.WriteLine($"FirstName : {dataRow["FirstName"]}");
+                        streamWriter.WriteLine($"LastName : {dataRow["LastName"]}");
+                        streamWriter.WriteLine($"Email : {dataRow["Email"]}");
+                        streamWriter.WriteLine($"Phone : {dataRow["Phone"]}");
+                        streamWriter.WriteLine($"Money : {dataRow["Money"]}");
+                        streamWriter.WriteLine();
+                    }
+               }
+                Console.WriteLine("Successfully wrote into the file");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        #endregion
+    }
+}
--- API training/Csharp/Bank Management System/Bank Management System/Program.cs
+++ API training/Csharp/Bank Management System/Bank Management System/Program.cs
@@ -3,208 +3,19 @@ using System.Data;
 
 namespace Bank_Management_System
 {
-    public class Bank
-    {
-        public int id = 1;
-        public int UserId { get; set; }
-        public string FirstName { get; set; }
-
-        public string LastName { get; set; }
-
-        public string Email { get; set; }
-
-        public string Phone { get; set; }
-
-        /// <summary>
-        ///     Create the data column if the data table is not exist and add the rows
-        ///     call display current user data function
-        /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="firstName"></param>
-        /// <param name="lastName"></param>
-        /// <param name="email"></param>
-        /// <param name="phone"></param>
-        public void CreateAccount(DataTable dataTable, string firstName, string lastName, string email, string phone)
-        {
-            if (!ColumnExists(dataTable, "FirstName"))
-            {
-                DataColumn columnUserId = new DataColumn("UserId", typeof(int));
-                DataColumn columnFName = new DataColumn("FirstName", typeof(string));
-                DataColumn columnLName = new DataColumn("LastName", typeof(string));
-                DataColumn columnEmail = new DataColumn("Email", typeof(string));
-                DataColumn columnPhone = new DataColumn("Phone", typeof(string));
-                DataColumn columnMoney = new DataColumn("Money", typeof(int));
-
-                dataTable.Columns.Add(columnUserId);
-                dataTable.Columns.Add(columnFName);
-                dataTable.Columns.Add(columnLName);
-                dataTable.Columns.Add(columnEmail);
-                dataTable.Columns.Add(columnPhone);
-                dataTable.Columns.Add(columnMoney);
-
-                DataColumn[] assignPrimaryKey = { dataTable.Columns["UserId"] };
-                dataTable.PrimaryKey = assignPrimaryKey;
-            }
-
-
-            DataRow row = dataTable.NewRow();
-            UserId = id;
-            row["UserId"] = id++;
-            row["FirstName"] = firstName;
-            row["LastName"] = lastName;
-            row["Email"] = email;
-            row["Phone"] = phone;
-            row["Money"] = 0;
-
-            dataTable.Rows.Add(row);
-
-            Console.WriteLine("Your account created sucessfully");
-
-            DisplayCurrentUserData(dataTable, UserId);
-        }
-
-        /// <summary>
-        /// find the datarow based on userid 
-        /// update the money - add money
-        /// display current user data
-        /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="money"></param>
-        public void DepositMoney(DataTable dataTable, int money)
-        {
-            Console.WriteLine("Money Started Deposit");
-
-            DataRow currentUser = dataTable.Rows.Find(UserId);
-            if (currentUser != null)
-            {
-                money += (int)currentUser["Money"];
-
-                currentUser["Money"] = money;
-                DisplayCurrentUserData(dataTable, UserId);
-            }
-            else
-            {
-                Console.WriteLine("Account does not exists");
-            }
-        }
-
-        /// <summary>
-        /// find the datarow based on userid 
-        /// update the money - decrease money
-        /// display current user data
-        /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="money"></param>
-        public void WithdrawMoney(DataTable dataTable, int money)
-        {
-            Console.WriteLine("Money Started Withdraw");
-            DataRow currentUser = dataTable.Rows.Find(UserId);
-            if (currentUser != null)
-            {
-                if ((int)currentUser["Money"] < money)
-                {
-                    Console.WriteLine("Insufficient Balance");
-                }
-                else
-                {
-                    currentUser["Money"] = (int)currentUser["Money"] - money;
-                    DisplayCurrentUserData(dataTable, UserId);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Account does not exists");
-            }
-        }
-
-        /// <summary>
-        ///     find the datarow based on usedid
-        ///     delete the row
-        /// </summary>
-        /// <param name="dataTable"></param>
-        public void CloseAccount(DataTable dataTable)
-        {
-            DataRow currentUser = dataTable.Rows.Find(UserId);
-            if (currentUser != null)
-            {
-                currentUser.Delete();
-                dataTable.AcceptChanges();
-
-                Console.WriteLine();
-                Console.WriteLine("your account is successfully deleted");
-                Console.WriteLine();
-            }
-            else
-            {
-                Console.WriteLine("Account does not exists");
-            }
-
-        }
-
-        /// <summary>
-        /// check whether data-base is exist or not
-        /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="columnName"></param>
-        /// <returns>bool</returns>
-        public static bool ColumnExists(DataTable dataTable, string columnName)
-        {
-            return dataTable.Columns.Contains(columnName);
-        }
-        
-        /// <summary>
-        /// Display current user data
-        /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="id"></param>
-        public static void DisplayCurrentUserData(DataTable dataTable, int id)
-        {
-            DataRow currentUser = dataTable.Rows.Find(id);
-            Console.WriteLine();
-            Console.WriteLine("*** Current user data ***");
-            Console.WriteLine();
-            Console.WriteLine($"UserId : {currentUser["UserId"]}");
-            Console.WriteLine($"FirstName : {currentUser["FirstName"]}");
-            Console.WriteLine($"LastName : {currentUser["LastName"]}");
-            Console.WriteLine($"Email : {currentUser["Email"]}");
-            Console.WriteLine($"Phone : {currentUser["Phone"]}");
-            Console.WriteLine($"Money : {currentUser["Money"]}");
-            Console.WriteLine();
-        }
-
-        /// <summary>
-        /// Display all user data
-        /// </summary>
-        /// <param name="dataTable"></param>
-        public static void DisplayAllUserData(DataTable dataTable)
-        {
-            // Iterating the data Table
-            foreach (DataRow dataRow in dataTable.Rows)
-            {
-                Console.WriteLine();
-                Console.WriteLine($"UserId : {dataRow["UserId"]}");
-                Console.WriteLine($"FirstName : {dataRow["FirstName"]}");
-                Console.WriteLine($"LastName : {dataRow["LastName"]}");
-                Console.WriteLine($"Email : {dataRow["Email"]}");
-                Console.WriteLine($"Phone : {dataRow["Phone"]}");
-                Console.WriteLine($"Money : {dataRow["Money"]}");
-                Console.WriteLine();
-            }
-        }
-    }
-
-
-    internal class Program
+    /// <summary>
+    /// Main entry point for the Bank Management System.
+    /// </summary>
+    public class Program
     {
         /// <summary>
-        ///     Take input from user
-        ///     call the createAccount method
+        /// Takes user input to create a new bank account and calls the createAccount method.
         /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="objBank"></param>
-        public static void CreateAccount(DataTable dataTable, Bank objBank)
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="objBank">Business logic object for banking operations.</param>
+        public static void CreateAccount(DataTable dataTable, BLBank objBank)
         {
-            Console.WriteLine("Account creating processing is strat...");
+            Console.WriteLine("Account creating processing is start...");
 
             Console.WriteLine("Enter your first name");
             objBank.FirstName = Console.ReadLine();
@@ -222,12 +33,11 @@ namespace Bank_Management_System
         }
 
         /// <summary>
-        ///     Take input money from user
-        ///     call the depositMoney method
+        /// Takes user input for the deposit amount and calls the depositMoney method.
         /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="objBank"></param>
-        public static void DepositMoney(DataTable dataTable, Bank objBank)
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="objBank">Business logic object for banking operations.</param>
+        public static void DepositMoney(DataTable dataTable, BLBank objBank)
         {
             Console.WriteLine("Enter the amount");
             int money = Convert.ToInt32(Console.ReadLine());
@@ -236,12 +46,11 @@ namespace Bank_Management_System
         }
 
         /// <summary>
-        ///     Take input money from user
-        ///     call the withdrawMoney method
+        /// Takes user input for the withdrawal amount and calls the withdrawMoney method.
         /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="objBank"></param>
-        public static void WithdrawMoney(DataTable dataTable, Bank objBank)
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="objBank">Business logic object for banking operations.</param>
+        public static void WithdrawMoney(DataTable dataTable, BLBank objBank)
         {
             Console.WriteLine("Enter the amount");
             int money = Convert.ToInt32(Console.ReadLine());
@@ -250,22 +59,22 @@ namespace Bank_Management_System
         }
 
         /// <summary>
-        ///  Call the closeAccount Method
+        /// Calls the closeAccount method to close a user account.
         /// </summary>
-        /// <param name="dataTable"></param>
-        /// <param name="objBank"></param>
-        public static void CloseAccount(DataTable dataTable, Bank objBank)
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="objBank">Business logic object for banking operations.</param>
+        public static void CloseAccount(DataTable dataTable, BLBank objBank)
         {
             objBank.CloseAccount(dataTable);
         }
-       
+
         /// <summary>
-        ///     based on user choice call the appropriate function
+        /// Based on user choice, calls the appropriate function for banking operations.
         /// </summary>
-        /// <param name="choice"></param>
-        /// <param name="dataTable"></param>
-        /// <param name="objBank"></param>
-        public static void selectChoice(int choice, DataTable dataTable, Bank objBank)
+        /// <param name="choice">User choice for banking operation.</param>
+        /// <param name="dataTable">DataTable to store user data.</param>
+        /// <param name="objBank">Business logic object for banking operations.</param>
+        public static void selectChoice(int choice, DataTable dataTable, BLBank objBank)
         {
 
             switch (choice)
@@ -284,11 +93,19 @@ namespace Bank_Management_System
                     break;
             }
         }
-     
+
+        /// <summary>
+        /// Main entry point for the Bank Management System.
+        /// </summary>
         public static void Main(string[] args)
         {
+            // DataTable to store user data
             DataTable dataTable = new DataTable("UserData");
-            Bank objBank = new Bank();
+
+            // Business logic for banking operations
+            BLBank objBank = new BLBank();
+
+            // Main program loop
             while (true)
             {
                 Console.WriteLine("Bank Management System");
@@ -298,15 +115,17 @@ namespace Bank_Management_System
                 Console.WriteLine("4. Close Account");
                 Console.WriteLine("5. Exit Bank");
 
+                // Get user choice
                 int choice = Convert.ToInt32(Console.ReadLine());
 
-                // if user enter the 5 break the while loop 
-                // display allUserDataTable from dataTable 
+                // if user enter the 5 break the while loop and display allUserDataTable from dataTable 
                 if (choice == 5)
                 {
-                    Bank.DisplayAllUserData(dataTable);
+                    BLBank.DisplayAllUserData(dataTable);
                     break;
                 }
+
+                // Call the appropriate method based on user choice
                 selectChoice(choice, dataTable, objBank);
             }
 

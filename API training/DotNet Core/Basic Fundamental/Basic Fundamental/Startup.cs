af9d1b6f2ef2144db8fc06753fdcc7d86f83a746
DevNakum <ndev2003@gmail.com>
2024-03-22 18:04:55 +0530

    Basic fundamental demo added

---
+++ API training/DotNet Core/Basic Fundamental/Basic Fundamental/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace Basic_Fundamental
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
---
+++ API training/DotNet Core/Basic WebAPI/Basic WebAPI/Program.cs
@@ -0,0 +1,13 @@
+using Basic_WebAPI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var startup = new Startup(builder.Configuration);
+        startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+        var app = builder.Build();
+        startup.Configure(app, builder.Environment); // calling Configure method
+    }
+}

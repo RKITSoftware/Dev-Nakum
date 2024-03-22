40e911fcad5f940a2e598aa23e6c338678e726d8
DevNakum <ndev2003@gmail.com>
2024-03-22 18:06:48 +0530

    routing- mvc demo added

---
+++ API training/DotNet Core/Routing/Routing/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Routing.Models;
+using System.Diagnostics;
+
+namespace Routing.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("home")]
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ API training/DotNet Core/Routing/Routing/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+    [Route("users")]
+    public class UsersController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{id}")]
+        public int Count(int id)
+        {
+            if (id < 0)
+            {
+                throw new Exception("Id must be greater then zero");
+            }
+            return id;
+        }
+    }
+}

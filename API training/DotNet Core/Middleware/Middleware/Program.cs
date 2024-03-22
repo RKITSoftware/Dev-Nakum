bfd1d4082c98bb750a49ef52df5266cea5527523
DevNakum <ndev2003@gmail.com>
2024-03-22 18:06:10 +0530

    middleware - mvc demo added

---
+++ API training/DotNet Core/Middleware/Middleware/Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+
+app.Use(async (context,next) =>
+{
+    await context.Response.WriteAsync("Hello world\n");
+    await next();
+});
+
+
+app.Run(async (context) =>
+{
+    await context.Response.WriteAsync("Hello world 2\n");
+});
+
+
+app.Run();

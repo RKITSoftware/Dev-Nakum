ac36754b1341e3e0f0acb92672dd7776b34fea2b
DevNakum <ndev2003@gmail.com>
2024-04-02 17:45:59 +0530

    added exception handling

---
+++ API training/DotNet Core/Exception_Handling/Exception_Handling/Controllers/CLExceptionsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exception_Handling.Controllers
+{
+    [ApiController]
+    public class CLExceptionsController : ControllerBase
+    {
+        [HttpGet("api/exception/{num1}/{num2}")]
+        public IActionResult GetValues(int num1,int num2)
+        {
+            try
+            {
+                return Ok(num1 / num2);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("api/error")]
+        public IActionResult Error()
+        {
+            return BadRequest("There is an error due to unhandled exception");
+        }
+    }
+}
---
+++ API training/DotNet Core/Exception_Handling/Exception_Handling/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Hosting;
+
+namespace Exception_Handling
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var startup = new Startup(builder.Configuration);
+            startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+            var app = builder.Build();
+            startup.Configure(app, builder.Environment); // calling Configure method
+        }
+    }
+}

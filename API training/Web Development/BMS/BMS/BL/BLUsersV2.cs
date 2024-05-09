84e6988e7d8ac451ca55f691f6f69baa26d80169
DevNakum <ndev2003@gmail.com>
2024-05-09 16:52:39 +0530

    changes are done in version 2

--- API training/Web Development/BMS/BMS/BL/BLUsersV2.cs
+++ API training/Web Development/BMS/BMS/BL/BLUsersV2.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace BMS.BL
 {
@@ -15,23 +16,33 @@ namespace BMS.BL
         /// </summary>
         private static List<UsersV2> _lstUsersV2 = new List<UsersV2>()
         {
-            new UsersV2(){Id=1, FullName="Dev Nakum",UserName="deven",Password="Abcd@123", Number="9856895874",Email="dev@gmail.com",Money=0,Role="A"},
-            new UsersV2(){Id=2, FullName="Kishan Nakum",UserName="kishan",Password="Abcd@123", Number="7458962351",Email="kishan@gmail.com",Money=0,Role="U"},
-        };
+            new UsersV2(){
+                Id=1, FullName="Dev Nakum",UserName="deven",Password="Abcd@123", Number="9856895874",Email="dev@gmail.com",Money=0,Role="A"},
+            };
+
 
         /// <summary>
         /// user id 
         /// </summary>
-        private static int _id = 3;
+        private static int _id = 2;
 
 
         /// <summary>
         /// Object of the users
         /// </summary>
         private UsersV2 _objUsersV2;
-
-       
         #endregion
+
+        #region Constructor
+        /// <summary>
+        /// initialize the object of the response model
+        /// </summary>
+        public BLUsersV2()
+        {
+            objResponse = new Response();
+        }
+        #endregion
+
 
         #region Public Member
         /// <summary>
@@ -42,9 +53,43 @@ namespace BMS.BL
 
         #region Public Properties
         /// <summary>
-        /// operation types - (I - Insert),(U - Update),(D - Delete)
+        /// operation types - (A - Add),(E - Edit),(D - Delete)
         /// </summary>
         public enmOperationTypes OperationTypes { get; set; }
+        #endregion
+
+
+
+        #region Private Method
+
+        /// <summary>
+        /// to check password is valid or noy based on regex
+        /// </summary>
+        /// <param name="password">user's password</param>
+        /// <returns>true if password is valid or else false</returns>
+        private bool IsValidPassword(string password)
+        {
+            //Minimum length(8 characters)
+            //At least one uppercase letter
+            //At least one lowercase letter
+            //At least one number
+            //At least one special character
+            string passwordRegex = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$";
+            return Regex.IsMatch(password, passwordRegex);
+        }
+
+        /// <summary>
+        /// to check user is exist or not 
+        /// </summary>
+        /// <typeparam name="T">data type </typeparam>
+        /// <param name="value"></param>
+        /// <param name="selector"></param>
+        /// <returns>true if user exist or else false</returns>
+        private bool IsUserExist<T>(T value, Func<UsersV2, T> selector)
+        {
+            return _lstUsersV2.Any(u => selector(u).Equals(value));
+        }
+
         #endregion
 
         #region Public Method
@@ -82,17 +127,38 @@ namespace BMS.BL
             objResponse = new Response();
             if (OperationTypes == enmOperationTypes.A)
             {
-                bool isUsers = _lstUsersV2.Any(u => u.UserName == _objUsersV2.UserName);
+                // to validate username is exists or not
+                bool isUsers = IsUserExist(_objUsersV2.UserName, u => u.UserName);
+
+                // to validate the phone number 
+                bool isValidNumber = _objUsersV2.Number.Length == 10;
+
+                // to validate the password
+                bool iSValidPassword = IsValidPassword(_objUsersV2.Password);
+
+                // response set based on validations
                 if (isUsers)
                 {
                     objResponse.IsError = true;
                     objResponse.Message = "Username already exists.";
                 }
+
+                if (!isValidNumber)
+                {
+                    objResponse.IsError = true;
+                    objResponse.Message = "Enter the valid phone number.";
+                }
+
+                if (!iSValidPassword)
+                {
+                    objResponse.IsError = true;
+                    objResponse.Message = "Enter the valid password.";
+                }
             }
 
             if (OperationTypes == enmOperationTypes.E)
             {
-                bool isUsers = _lstUsersV2.Any(u => u.Id == _objUsersV2.Id);
+                bool isUsers = IsUserExist(_objUsersV2.Id, u => u.Id);
                 if (!isUsers)
                 {
                     objResponse.IsError = true;
@@ -109,11 +175,10 @@ namespace BMS.BL
         /// <returns>Response model</returns>
         public Response ValidationOnDelete(int id)
         {
-            objResponse = new Response();
             if (OperationTypes == enmOperationTypes.D)
             {
-                UsersV2 user = _lstUsersV2.FirstOrDefault(u => u.Id == id);
-                if (user == null)
+                bool isUser = IsUserExist(id, u => u.Id);
+                if (!isUser)
                 {
                     objResponse.IsError = true;
                     objResponse.Message = "user is not exists.";
@@ -121,7 +186,7 @@ namespace BMS.BL
                 else
                 {
                     _objUsersV2 = new UsersV2();
-                    _objUsersV2 = user;
+                    _objUsersV2.Id = id;
                 }
             }
             return objResponse;
@@ -133,16 +198,25 @@ namespace BMS.BL
         /// <returns>Response model</returns>
         public Response Save()
         {
-            objResponse = new Response();
             if (OperationTypes == enmOperationTypes.A)
             {
-                _lstUsersV2.Add(_objUsersV2);
+                try
+                {
+                    _lstUsersV2.Add(_objUsersV2);
+                }
+                catch (Exception ex)
+                {
+                    objResponse.IsError = true;
+                    objResponse.Message = ex.Message;
+                    throw ex;
+                }
 
                 objResponse.Message = "User added successfully";
             }
 
             if (OperationTypes == enmOperationTypes.E)
             {
+                // get the user object from the list and update it with _objUsersV1 object
                 UsersV2 user = _lstUsersV2.FirstOrDefault(u => u.Id == _objUsersV2.Id);
 
                 user.Email = _objUsersV2.Email;
@@ -160,25 +234,25 @@ namespace BMS.BL
         /// <returns>response model</returns>
         public Response Login(UsersV2 objUsersV2)
         {
-            objResponse = new Response();
-            UsersV2 userV1 = _lstUsersV2.FirstOrDefault(u => u.UserName == objUsersV2.UserName && u.Password == objUsersV2.Password);
+            UsersV2 userV2 = _lstUsersV2.FirstOrDefault(u => u.UserName == objUsersV2.UserName && u.Password == objUsersV2.Password);
 
-            if (userV1 != null)
+            if (userV2 != null)
             {
                 BLAuth objBLAuth = new BLAuth();
-                string token = objBLAuth.GenerateJWT(userV1.Id, userV1.UserName, userV1.Email, userV1.Role);
+                // get the token
+                string token = objBLAuth.GenerateJWT(userV2.Id, userV2.UserName, userV2.Email, userV2.Role);
+
+                object loginData = new
+                {
+                    token,
+                    userV2.Role
+                };
+
+                // Add the user details in cache
+                BLCache.Add("user", userV2);
+
                 objResponse.Message = "User logged in successfully";
-
-                DataTable dt = new DataTable();
-                dt.Columns.Add("token", typeof(string));
-                dt.Columns.Add("role", typeof(string));
-
-                DataRow row = dt.NewRow();
-                row["token"] = token;
-                row["role"] = userV1.Role;
-
-                dt.Rows.Add(row);
-                objResponse.Data = dt;
+                objResponse.Data = loginData;
             }
             else
             {
@@ -195,22 +269,27 @@ namespace BMS.BL
         /// <returns>response model</returns>
         public Response GetUser(int id)
         {
-            objResponse = new Response();
-            UsersV2 user = _lstUsersV2.FirstOrDefault(u => u.Id == id);
-
-            if (user == null)
+            // try to get the user from cache 
+            object userFromCache = BLCache.Get("user");
+            if (userFromCache != null)
             {
-                objResponse.IsError = true;
-                objResponse.Message = "User is not found";
-
-                return objResponse;
+                objResponse.Data = userFromCache;
             }
-            List<UsersV2> _lstUser = new List<UsersV2>()
+            else
             {
-                user
-            };
+                UsersV2 user = _lstUsersV2.FirstOrDefault(u => u.Id == id);
 
-            objResponse.Data = _lstUser.ToDataTable();
+                if (user == null)
+                {
+                    objResponse.IsError = true;
+                    objResponse.Message = "User is not found";
+
+                    return objResponse;
+                }
+
+                objResponse.Data = user;
+
+            }
             return objResponse;
         }
 
@@ -232,7 +311,7 @@ namespace BMS.BL
         public Response GetAllUser()
         {
             objResponse = new Response();
-            objResponse.Data = _lstUsersV2.ToDataTable();
+            objResponse.Data = _lstUsersV2;
             return objResponse;
         }
 
@@ -243,9 +322,7 @@ namespace BMS.BL
         /// <returns>Response model</returns>
         public Response DeleteUser()
         {
-            objResponse = new Response();
-
-            _lstUsersV2.Remove(_objUsersV2);
+            _lstUsersV2.RemoveAll(u => u.Id == _objUsersV2.Id);
             objResponse.Message = "User is deleted successfully";
             return objResponse;
         }
